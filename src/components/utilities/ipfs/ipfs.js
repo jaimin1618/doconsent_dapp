a5f4c2ca6cf11af9057ae2c27af1a4b93f111ad9
jaimin1618 <jaimin.chokhawala@gmail.com>
2023-02-07 22:21:51 +0530

    updating IPFS read/write scripts, still NOT working due to IPFS Lock issue. But this solution works on each app refresh

--- src/components/utilities/ipfs/ipfs.js
+++ src/components/utilities/ipfs/ipfs.js
@@ -1,17 +1,35 @@
 import { create } from "ipfs";
 
+function randomInt(start, end) {
+  return Math.floor(Math.random() * (end - start + 1) + start);
+}
+
+var ready = false;
+async function getNode(config) {
+  if (!ready) {
+    const node = await create(config);
+    ready = true;
+    return node;
+  }
+  return null;
+}
+
 async function upload_data(data) {
-  const node = await create();
+  const node = await getNode({ repo: "ok" + Math.random() });
   const file = await node.add(JSON.stringify(data));
   return file.cid.toString();
 }
 
 async function read_data(cid) {
+  const random = randomInt(1, 100);
+  console.log(random);
+  const node = await getNode({ repo: "ok" + random });
+
   if (cid === "" || cid === null) {
     throw Error("found empty CID for data read");
   }
 
-  const node = await create();
+  // const node = await create({ repo: "ok" + Math.random() });
   const decoder = new TextDecoder();
   const stream = await node.cat(cid);
   let data = "";
